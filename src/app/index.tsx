87db8fd5567267af517f43483abdae96f4d8338b
Tyler Bush <bushtyler05@gmail.com>
2026-05-02 22:08:31 -0400

    global styles and header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { globalStyles } from "@/styles/global";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Block It</Text>
+      <Text style={styles.date}>Today</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  date: {
+    fontSize: 14,
+    color: '#a0a0b0',
+    marginTop: 4,
+    marginBottom: 30,
+  }
 });
