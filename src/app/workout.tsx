cb6db3fb7f9f3801cc742900cadea2c1a32162fd
Tyler Bush <bushtyler05@gmail.com>
2026-05-05 21:10:57 -0400

    rename workout page

--- src/app/workout.tsx
+++ src/app/workout.tsx
@@ -1,10 +1,10 @@
 import { globalStyles } from '@/styles/global';
 import { ScrollView, Text } from 'react-native';
 
-export default function Workouts() {
+export default function Workout() {
   return (
     <ScrollView style={globalStyles.container}>
-      <Text style={globalStyles.title}>Workouts</Text>
+      <Text style={globalStyles.title}>Workout</Text>
     </ScrollView>
   );
 }
